fb2da319b91bfc41b1837d08c41f077b271712f8
Nikolay Atanasov <atanasov@officernd.com>
2020-09-02 08:27:49 +0300

    Add simple react app

---
+++ server/frontend/src/components/workouts/Workouts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Workouts extends Component {
+    render() {
+        return (
+            <div>
+                <h1>workouts list</h1>
+            </div>
+        )
+    }
+}
+
+export default Workouts
