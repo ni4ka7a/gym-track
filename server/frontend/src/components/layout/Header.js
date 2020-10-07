f77ab5b53ffac6313b267c9747d2134593c3dd68
Nikolay Atanasov <atanasov@officernd.com>
2020-10-07 08:59:17 +0300

    Fix navbar

--- server/frontend/src/components/layout/Header.js
+++ server/frontend/src/components/layout/Header.js
@@ -1,38 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { connect }  from 'react-redux';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
 export class Header extends Component {
+  static propTypes = {
+    auth: PropTypes.object.isRequired,
+    logout: PropTypes.func.isRequired,
+  };
 
-    static propTypes =  {
-        auth: PropTypes.object.isRequired,
-        logout: PropTypes.func.isRequired
-    };
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
+    const authLinks = (
+      <ul className="navbar-nav ml-auto mt- 2 mt-lg-0">
+        <span className="navbar-text mr-3">
+          <strong>{user ? `Welcome ${user.username}` : ''}</strong>
+        </span>
+        <li className="nav-item">
+          <button
+            onClick={this.props.logout}
+            className="nav-link btn btn-info btn-sm text-light"
+          >
+            Logout
+          </button>
+        </li>
+      </ul>
+    );
 
-    render() {
-        const { isAuthenticated, user }  = this.props.auth;
-        const authLinks = (
-            <ul className="navbar-nav mr-auto mt- 2 mt-lg-0">
-                <span className="navbar-text mr-3">
-                    <strong>{ user?  `Welcome ${user.username}` : "" }</strong>
-                </span>
-                <li className="nav-item">
-                    <button onClick={this.props.logout} className="nav-link btn btn-info btn-sm text-light">Logout</button>
-                </li>
-            </ul>
-        );
-
-        const  guestLinks = (
-            <ul className="navbar-nav mr-auto mt- 2 mt-lg-0">
-                <li className="nav-item">
-                    <Link to="/register" className="nav-link">Register</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/login" className="nav-link">Login</Link>
-                </li>
-            {/* <li className="nav-item active">
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            Register
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </li>
+        {/* <li className="nav-item active">
                 <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
@@ -41,29 +49,60 @@ export class Header extends Component {
             <li className="nav-item">
                 <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
             </li> */}
-            </ul>
-        );
+      </ul>
+    );
 
-        return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                {/* <div className="container"> */}
-                <div>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <a className="navbar-brand" href="#">Gym track</a>
-                        {/* the UL here? */}
-                        { isAuthenticated ? authLinks : guestLinks  }
-                    </div>
-                </div>
-            </nav>
-        )
-    }
+    return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Gym Track
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          {isAuthenticated && (
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">
+                  Home <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Workouts
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Routines
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Calendar
+                </a>
+              </li>
+            </ul>
+          )}
+          {isAuthenticated ? authLinks : guestLinks}
+        </div>
+      </nav>
+    );
+  }
 }
 
-const mapStateToProps  = state => ({
-    auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, { logout })(Header);
